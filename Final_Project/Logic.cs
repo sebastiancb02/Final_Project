2f36240fca0bb29d22907e592845543cbd4ceb17
Seb <sebastiancopara8@gmail.com>
2026-09-20 11:06:48 +0200

    Create a method in logic to match users by location

--- Final_Project/Logic.cs
+++ Final_Project/Logic.cs
@@ -2,5 +2,34 @@ namespace Final_Project;
 
 public class Logic
 {
-    
+    public static Dictionary<string, List<User>> MatchUsersByLocation(List<User> listOfUsers)
+    {
+        var usersPerLocation = new Dictionary<string, List<User>>();
+        //initializing dictionary with all the available locations
+        foreach (var user in listOfUsers)
+        {
+            foreach (var trip in user.trips)
+            {
+                usersPerLocation.Add(trip.city,new List<User>());
+            }
+        }
+
+        //populating the user list per location
+        foreach (var location in usersPerLocation.Keys) //first iteration is Stockholm
+        {
+            foreach (var user in listOfUsers)
+            {
+                foreach (var trip in user.trips)
+                {
+                    if (location == trip.city)
+                    {
+                        usersPerLocation[location].Add(user);
+                    }
+                }
+            }
+        }
+        // usersPerLocation contains all the available locations, and for each location it contains the list of users going to this location
+        
+        return usersPerLocation;
+    }
 }
